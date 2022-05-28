6a38d40ab5fd705394463f39332221abd616f388
jokerav <50701756+jokerav@users.noreply.github.com>
2022-05-28 16:24:49 +0300

    Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import Section from './Section/section';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
 export class App extends Component {
   static defaultProps = {
     good: 0,
@@ -17,8 +18,19 @@ export class App extends Component {
       [option]: prevState[option] + 1,
     }));
   };
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    if (this.countTotalFeedback() === 0) {
+      return 0;
+    } else return Math.round((good / this.countTotalFeedback()) * 100);
+  };
 
   render() {
+    const { good, neutral, bad } = this.state;
     return (
       <div
         style={{
@@ -35,6 +47,15 @@ export class App extends Component {
             options={['good', 'neutral', 'bad']}
             onLeaveFeedback={this.leaveFeedback}
           ></FeedbackOptions>
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+          ></Statistics>
         </Section>
       </div>
     );
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,10 @@
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
+  <div>
+    <p>Good: {good}</p>
+    <p>Neutral:{neutral}</p>
+    <p>Bad: {bad}</p>
+    <p>Total: {total}</p>
+    <p>Posotive feedback: {positivePercentage}%</p>
+  </div>
+);
+export default Statistics;
